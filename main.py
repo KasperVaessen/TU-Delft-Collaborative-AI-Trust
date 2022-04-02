b47403fb97b4ab08e87049c095ab59bacb1d6091
Max Le Blansch <m.l.leblansch@student.tudelft.nl>
2022-04-02 23:35:34 +0200

    Have all agents run in same level

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from agents1.Team36AgentLiar import LiarAgent
 from agents1.Team36AgentLazy import LazyAgent
+from agents1.Team36AgentColorblind import ColorBlindAgent
+from agents1.Team36AgentStrong import StrongAgent
 from agents1.Team36BaseAgent import BaseAgent
 from bw4t.BW4TWorld import BW4TWorld
 from bw4t.statistics import Statistics
 from agents1.BW4TBaselineAgent import BaseLineAgent
 from agents1.BW4THuman import Human
-from agents1.Team36ColorblindAgent import ColorBlindAgent
 
 
 """
@@ -19,9 +20,11 @@ if __name__ == "__main__":
         # {'name':'agent1', 'botclass':BaseLineAgent, 'settings':{'slowdown':10}},
         # {'name':'agent2', 'botclass':BaseLineAgent, 'settings':{}},
         {'name':'liar', 'botclass':LiarAgent, 'settings':{}},
-        # {'name':'lazy', 'botclass':LazyAgent, 'settings':{}},
-        {'name':'Base', 'botclass':BaseAgent, 'settings':{}},
-        {'name':'Base2', 'botclass':BaseAgent, 'settings':{}},
+        {'name':'lazy', 'botclass':LazyAgent, 'settings':{}},
+        {'name': 'strong', 'botclass': StrongAgent, 'settings': {}},
+        {'name': 'colorblind', 'botclass': ColorBlindAgent, 'settings': {}},
+        # {'name':'Base', 'botclass':BaseAgent, 'settings':{}},
+        # {'name':'Base2', 'botclass':BaseAgent, 'settings':{}},
         # {'name':'human', 'botclass':Human, 'settings':{}}
         ]
 
